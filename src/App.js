842f15a73a1208aae4d641cc8d2cbde765768fed
ghady101 <ghadygergie0@gmail.com>
2022-12-26 13:46:44 +0200

    save change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import "./App.css";
-import Footer from "./Component/Footer";
-import Header from "./Component/Header";
-import Home from "./Component/Home";
-import SearchPage from "./Component/SearchPage";
-import { BrowserRouter as Router, Route, Router, Link } from "react-router-dom";
 import React from "react";
 import "./App.css";
-import Home from "./Component/Home";
-import Header from "./Component/Header";
 import Footer from "./Component/Footer";
+import Header from "./Component/Header";
+import Home from "./Component/Home";
 import SearchPage from "./Component/SearchPage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 function App() {
   return (
     // BEM
