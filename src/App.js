082f95c0d9f359f1d16402c6c718109837783724
mohammedkhachab <you@example.com>
2022-12-26 12:58:48 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import "./App.css";
-import Footer from "./Component/Footer";
-import Header from "./Component/Header";
-import Home from "./Component/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import Home from './Component/Home'
+import Header from './Component/Header'
+import Footer from './Component/Footer'
+import SearchPage from './Component/SearchPage'
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+
+    // BEM
     <div className="app">
       <Router>
         <Header />
-
-        <Home />
-
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
         <Footer />
-      </Router>
+      </ Router>
     </div>
   );
 }
